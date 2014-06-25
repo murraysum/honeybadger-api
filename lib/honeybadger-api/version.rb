ee9276a6d63bbc051ee9c1cdf97483f4ba3cc277
Murray Summers <murray.sum@gmail.com>
2014-06-25 23:12:26 +0100

    Bump version for first public release.

--- lib/honeybadger-api/version.rb
+++ lib/honeybadger-api/version.rb
@@ -1,5 +1,5 @@
 module Honeybadger
   module Api
-    VERSION = "0.0.1"
+    VERSION = "1.0.0"
   end
 end
--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Honeybadger::Api::VERSION" do
-  it "should be version 0.0.1" do
-    Honeybadger::Api::VERSION.should == "0.0.1"
+  it "should be version 1.0.0" do
+    Honeybadger::Api::VERSION.should == "1.0.0"
   end
 end
